fb32990a31445bd015a67240782cd77835d6b2f5
Jeremy Cole <jeremy@jcole.us>
2021-07-03 15:24:00 -0700

    Also rubocop spec

--- spec/weather_gov_spec.rb
+++ spec/weather_gov_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.describe WeatherGov do
-  it 'has a version number' do
+  it "has a version number" do
     expect(WeatherGov::VERSION).not_to be nil
   end
 end
